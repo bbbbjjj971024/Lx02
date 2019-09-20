b148d008418704b0f63fb66a710d4c2f42ec7482
bbbbjjj971024 <54525087+bbbbjjj971024@users.noreply.github.com>
2019-09-20 09:18:33 +0800

    Add files via upload

---
+++ Model.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Model
+{
+    public class UserUnfo
+    {
+        public string userName {get;set;}
+        public string passWord {get;set;}
+        public string qq {get;set;}
+        public string email {get;set;}
+        public string type {get;set;}
+        public string userImg { get; set; }
+    }
+    public class UserUnfoNo : UserUnfo
+    {
+        public int num { get; set; }
+    }
+    public class Page {
+        public int pageIndex { get; set; }
+        public int pageSize { get; set; }
+    }
+}
---
+++ UserInfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MySql.Data.MySqlClient;
+using System.Data;
+using Dapper;
+namespace DAL
+{
+    public class UserInfo
+    {
+        private UserInfo() { }
+        public static UserInfo _instance = new UserInfo();
+        public static UserInfo Instance
+        {
+            get
+            {
+                return _instance;
+            }
+        }
+  
+    }
+}
